f6ae6454ccf93f3d1cc491fb2e135d696f5ebde7
Jonathan Bradshaw <lavin@posteo.de>
2017-03-18 16:50:50 -0700

    Complete test "Set memo as first array item if no memo is passed in"

--- src/underbar.js
+++ src/underbar.js
@@ -181,12 +181,12 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
-    if(accumulator === 'undefined'){
+    if(typeof accumulator === 'undefined'){
       accumulator = collection[0];
     }
 
     for (var i = 0; i < collection.length; i++) {
-      accumulator = iterator(accumulator, collection[i]);
+      accumulator = iterator(accumulator,collection[i]);
     }
 
     return accumulator;
