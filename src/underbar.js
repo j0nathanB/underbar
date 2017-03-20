80a28acbdf5d199c46a547a81d4d1d7c3a0f907e
Jonathan Bradshaw <lavin@posteo.de>
2017-03-19 22:31:56 -0700

    Complete "extends" and "default" tests

--- src/underbar.js
+++ src/underbar.js
@@ -210,7 +210,7 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    var iterator = iterator || _.identity;
+    iterator = iterator || _.identity;
 
     return  !!_.reduce(collection, function(accumulator, item){
       return accumulator && iterator(item);
@@ -220,6 +220,11 @@
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
+    iterator = iterator || _.identity;
+
+    return  !!_.reduce(collection, function(accumulator, item){
+      return accumulator || iterator(item);
+    }, false);
     // TIP: There's a very clever way to re-use every() here.
   };
 
@@ -243,11 +248,29 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    //console.log('*** obj: ' + JSON.stringify(obj) + '\n' + 'arguments: ' + JSON.stringify(arguments))
+    _.each(arguments, function(passedInObject){
+     //console.log('passedInObject: ' + JSON.stringify(passedInObject))
+      _.each(passedInObject, function(value, key){
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    //console.log('*** obj: ' + JSON.stringify(obj) + '\n' + 'arguments: ' + JSON.stringify(arguments))
+    _.each(arguments, function(passedInObject){
+      //console.log('passedInObject: ' + JSON.stringify(passedInObject))
+      _.each(passedInObject, function(value, key){
+        if(obj[key] === undefined){
+          obj[key] = value;
+        }
+      });
+    });
+    return obj;
   };
 
 
